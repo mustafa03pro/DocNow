448d9af2f716a3e31c86c24a4f377f3abc55c698
Mohd Mustafa Ahmed <mustafamohdvvip@gmail.com>
2025-07-17 19:43:30 +0530

    Add files via upload

--- utils.js
+++ utils.js
@@ -13,34 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import { DiagAPI } from '../api/diag';
-import { BaggageImpl } from './internal/baggage-impl';
-import { baggageEntryMetadataSymbol } from './internal/symbol';
-const diag = DiagAPI.instance();
-/**
- * Create a new Baggage with optional entries
- *
- * @param entries An array of baggage entries the new baggage should contain
- */
-export function createBaggage(entries = {}) {
-    return new BaggageImpl(new Map(Object.entries(entries)));
-}
-/**
- * Create a serializable BaggageEntryMetadata object from a string.
- *
- * @param str string metadata. Format is currently not defined by the spec and has no special meaning.
- *
- */
-export function baggageEntryMetadataFromString(str) {
-    if (typeof str !== 'string') {
-        diag.error(`Cannot create baggage metadata from unknown type: ${typeof str}`);
-        str = '';
-    }
-    return {
-        __TYPE__: baggageEntryMetadataSymbol,
-        toString() {
-            return str;
-        },
-    };
+import { TraceStateImpl } from './tracestate-impl';
+export function createTraceState(rawTraceState) {
+    return new TraceStateImpl(rawTraceState);
 }
 //# sourceMappingURL=utils.js.map
